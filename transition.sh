ee974208285d2316cc5619ed19919e43b866acbd
Thomas NJ Shadwell <thomas.shadwell@googlemail.com>
2013-06-18 12:28:38 +0100

    Useless

--- transition.sh
+++
@@ -1,9 +0,0 @@
-gofmt -d -e -s \
-	-r 'options -> UnderlyingOptions' \
-	-r 'closer -> Closer' \
-	-r 'database -> UnderlyingDatabase'\
-	-r 'writeOptions -> UnderlyingWriteOptions'\
-	-r 'readOptions -> UnderlyingReadOptions'\
-	-r 'writeBatch -> UnderlyingWriteBatch'\
-	-r 'cache -> UnderlyingCache'\
-	.
